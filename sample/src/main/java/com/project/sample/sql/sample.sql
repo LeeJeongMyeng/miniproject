117a8422f8af317b3fab97e954574c1bab7f2b0a
이정명(Lee Jeong Myeong) <112403307+LeeJeongMyeng@users.noreply.github.com>
2023-08-03 14:22:32 +0900

    mysql연동

---
+++ sample/src/main/java/com/project/sample/sql/sample.sql
@@ -0,0 +1,23 @@
+# schema 사용 선언
+use sample;
+# 한글설정
+alter database sample default character set utf8 collate  utf8_general_ci;
+alter table user3 default charset = utf8;
+ALTER TABLE use1r3 CONVERT TO CHARSET UTF8;
+
+
+create table user3(
+    no int not null auto_increment primary key ,
+    name varchar(100) not null ,
+    email varchar(200) not null ,
+    pwd varchar(100) not null ,
+    gender boolean not null default 1,
+    del boolean not null default 0,
+    regData datetime not null default now()
+);
+
+insert into user3 (name,email,pwd,gender) value ('사용자','user@email.com','1111',1);
+
+
+select *
+from user3;
