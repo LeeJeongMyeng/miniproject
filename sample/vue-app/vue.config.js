8fca36bbf862be0f9cb9787f813723759f4e4743
이정명 <aoddl56@nate.com>
2023-08-17 11:13:17 +0900

    AASD

--- sample/vue-app/vue.config.js
+++ sample/vue-app/vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  outputDir: '../sample/src/main/resources/static'
 })
 
 
