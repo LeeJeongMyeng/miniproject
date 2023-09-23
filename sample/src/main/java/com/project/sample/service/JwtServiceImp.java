acb5009856c04481de770e4a3739d4f8af42e98e
이정명(Lee Jeong Myeong) <112403307+LeeJeongMyeng@users.noreply.github.com>
2023-09-23 21:45:41 +0900

    ㅇDDD

--- sample/src/main/java/com/project/sample/service/JwtServiceImp.java
+++ sample/src/main/java/com/project/sample/service/JwtServiceImp.java
@@ -23,21 +23,25 @@ public class JwtServiceImp implements JwtService{
     public String getToken(String key, Object value) {
 
         Date expTime = new Date();
-        //millisecond기준 => 30분
-        expTime.setTime(expTime.getTime()+ 1000*60*30);
+        //토큰의 유효시간 millisecond기준 => 30분
+        expTime.setTime(expTime.getTime()+ 1000*60*5);
         //key를 byte로 변환
         System.out.println(secretkey);
         byte[] secretkeyByte = DatatypeConverter.parseBase64Binary(secretkey);
+
+        //알고리즘을과 문자열로된 key로 비밀 키를 구성하는 key클래스
         Key signKey = new SecretKeySpec(secretkeyByte, SignatureAlgorithm.HS256.getJcaName());
 
+        //JWT의 Header 정보표시할것 정의
         Map<String, Object> headerMap = new HashMap<>();
         headerMap.put("typ", "JWT");
         headerMap.put("alg", "HS256");
 
-
+        //Claims => JWT의 Body부분
         Map<String, Object> ClaimsMap = new HashMap<>();
         ClaimsMap.put(key,value);
 
+        // 위 header/claims를 통해서 jwt토큰 생성
         JwtBuilder builder = Jwts.builder()
                 .setHeader(headerMap) //헤더정보
                 .setClaims(ClaimsMap) //클레임정보 담는곳
